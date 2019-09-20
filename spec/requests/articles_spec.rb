0b38f4dd64dd5fa596ce0a39a5b577fa085099f4
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-20 18:11:18 +0530

    added delete spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,8 +2,44 @@ require "rails_helper"
 
 RSpec.describe "Articles", type: :request do 
     before do 
-        @article = Article.create(title: "first article", body: "body of first article" )
+        @john = User.create(email: "john@example.com", password: "password")
+        @jane = User.create(email: "jane@example.com", password: "password")
+        @article = Article.create!(title: "first article", body: "body of first article", user: @john )
     end
+
+    describe 'GET/articles/:id/edit' do 
+        context 'with non signed in user' do 
+            before { get "/articles/#{@article.id}/edit" }
+
+            it "redirects to do the signup" do 
+                expect(response.status).to eq 302
+                flash_message = "You need to sign in or sign up before continuing."
+                expect(flash[:alert]).to eq flash_message
+            end
+        end 
+
+        context 'with signed in user who is non creator' do 
+            before do 
+                login_as(@jane)
+                get "/articles/#{@article.id}/edit"
+            end
+            it "redirects to do the homepage" do
+                expect(response.status).to eq 302
+                flash_message = "You can only edit your own article"
+                expect(flash[:alert]).to eq flash_message
+            end
+        end
+
+        context 'with signed in user who is the creator' do 
+            before do 
+                login_as(@john)
+                get "/articles/#{@article.id}/edit"
+            end
+            it "sucessfully edits articles" do
+                expect(response.status).to eq 200
+            end
+        end
+    end 
 
     describe 'GET /articles/:id' do 
         context 'with existing article' do
@@ -22,5 +58,41 @@ RSpec.describe "Articles", type: :request do
                 expect(flash[:alert]).to eq flash_message
             end 
         end
-    end 
+    end
+
+    describe 'DELETE/articles/:id' do
+        context 'with non signed in user' do 
+            before { delete "/articles/#{@article.id}" }
+
+            it "redirects to do the signup" do 
+                expect(response.status).to eq 302
+                flash_message = "You need to sign in or sign up before continuing."
+                expect(flash[:alert]).to eq flash_message
+            end
+        end
+
+        context 'with signed in user who is non creator' do 
+            before do 
+                login_as(@jane)
+                delete "/articles/#{@article.id}"
+            end
+            it "redirects to do the homepage" do
+                expect(response.status).to eq 302
+                flash_message = "You can only delete your own article"
+                expect(flash[:alert]).to eq flash_message
+            end
+        end
+
+        context 'with signed in user who is the creator' do 
+            before do 
+                login_as(@john)
+                delete "/articles/#{@article.id}"
+            end
+            it "sucessfully deletes articles" do
+                expect(response.status).to eq 302
+                flash_message = "Article has been deleted"
+                expect(flash[:success]).to eq flash_message
+            end
+        end
+    end
 end 
