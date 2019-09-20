603b974aa8c5bcb25023527d597d64d9931294ae
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-20 17:16:57 +0530

    Show new article to only signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -18,6 +18,33 @@ RSpec.feature "Listing Articles" do
         expect(page).to have_link(@article2.title) 
     end
 
+    scenario "with articles created and user not signed in" do 
+        visit "/"
+
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body)
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+        expect(page).to have_link(@article1.title) 
+        expect(page).to have_link(@article2.title) 
+        expect(page).not_to have_link("New Article") 
+
+    end
+
+    scenario "with articles created and user signed in" do 
+        login_as(@john)
+        visit "/"
+
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body)
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+        expect(page).to have_link(@article1.title) 
+        expect(page).to have_link(@article2.title) 
+        expect(page).to have_link("New Article") 
+
+    end
+
     scenario "A user has no articles" do 
         Article.delete_all
         visit "/"
