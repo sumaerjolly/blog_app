0b5b4b299f77487bce55ac778a0b94e9f83d1a73
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-20 17:25:08 +0530

    Update spec to make sure owner sees edit and delete

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,17 +3,46 @@ require "rails_helper"
 RSpec.feature "Showing an article" do
     before do 
         @john = User.create(email: "john@example.com", password: "password")
-        login_as(@john)
+        @jane = User.create(email: "jane@example.com", password: "password")
         @article = Article.create(title: "first article", body: "body of first article", user: @john )
     end
 
-    scenario "A user shows an article" do 
+    scenario "To non singed in user hide the Edit and Delete buttons" do 
         visit "/"
         click_link @article.title
 
         expect(page).to have_content(@article.title)
         expect(page).to have_content(@article.body)
         expect(current_path).to eq(article_path(@article))
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+
+    end
+
+    scenario "To non creator of article hide the Edit and Delete buttons" do 
+        login_as(@jane)
+        visit "/"
+        click_link @article.title
+
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(current_path).to eq(article_path(@article))
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+
+    end
+
+    scenario "Signed in user who is the creator can edit and delete" do 
+        login_as(@john)
+        visit "/"
+        click_link @article.title
+
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(current_path).to eq(article_path(@article))
+        expect(page).to have_link("Edit Article")
+        expect(page).to have_link("Delete Article")
+
     end
 
 end
